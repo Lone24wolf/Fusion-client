0cce29c3f605ff9b91368def72aa1a402d18a5be
nishanth <nishanth100000@gmail.com>
2024-11-21 01:26:13 +0530

    update bonafide status

--- src/routes/otheracademicRoutes/index.jsx
+++ src/routes/otheracademicRoutes/index.jsx
@@ -2,3 +2,6 @@ import { host } from "../globalRoutes";
 
 export const Leave_Form_Submit = `${host}/otheracademic/api/leave-form-submit/`;
 export const Bonafide_Form_Submit = `${host}/otheracademic/api/bonafide-form-submit/`;
+export const Fetch_Pending_Bonafide_Request = `${host}/otheracademic/api/admin-bonafide-requests/`;
+export const Update_Bonafide_Status = `${host}/otheracademic/api/admin-updates/`;
+export const Get_Bonafide_Status = `${host}/otheracademic/api/bonafide-status/`;

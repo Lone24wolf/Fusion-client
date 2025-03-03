4addee400ec692a4fa13b3b1ee2446b32348b86c
Abhyuday Singh <abhyudaysingh1729@gmail.com>
2025-03-03 16:14:25 +0530

    examination changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import LoginPage from "./pages/login";
 import ForgotPassword from "./pages/forgotPassword";
 import AcademicPage from "./Modules/Academic/index";
 import ValidateAuth from "./helper/validateauth";
-
+import Examination from "./Modules/Examination/examination";
 export default function App() {
   const location = useLocation();
   return (
@@ -50,6 +50,7 @@ export default function App() {
         />
         <Route path="/accounts/login" element={<LoginPage />} />
         <Route path="/reset-password" element={<ForgotPassword />} />
+        <Route path="/examination" element={<Examination/>}/>
       </Routes>
     </MantineProvider>
   );

4128cbf0c1e2ec69fa95e67af767703fd08bdeaa
Akash Kumar Sah <akashsah2003@gmail.com>
2025-03-15 17:42:12 +0530

    feat: Integrated Final Registration with backend

--- src/routes/academicRoutes/index.jsx
+++ src/routes/academicRoutes/index.jsx
@@ -6,3 +6,5 @@ export const addCalendarRoute = `${host}/aims/api/add-calendar/`;
 export const deleteCalendarRoute = `${host}/aims/api/delete-calendar/`;
 export const nextSemCoursesRoute = `${host}/academic-procedures/api/stu/next_sem_courses/`;
 export const currentCourseRegistrationRoute = `${host}/academic-procedures/api/stu/current_courseregistration/`;
+export const finalRegistrationPageRoute = `${host}/academic-procedures/api/stu/finalregistrationpage/`;
+export const finalRegistrationRoute = `${host}/academic-procedures/api/stu/final_registration/`;

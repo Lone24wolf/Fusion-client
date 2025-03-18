8d2392c1a21eecc88a5a42161a9371e07357cf0d
Akash Kumar Sah <akashsah2003@gmail.com>
2025-03-18 15:44:27 +0530

    feat(verify_registration): Added and modified apis for verify registration

--- src/routes/academicRoutes/index.jsx
+++ src/routes/academicRoutes/index.jsx
@@ -8,3 +8,7 @@ export const nextSemCoursesRoute = `${host}/academic-procedures/api/stu/next_sem
 export const currentCourseRegistrationRoute = `${host}/academic-procedures/api/stu/current_courseregistration/`;
 export const finalRegistrationPageRoute = `${host}/academic-procedures/api/stu/finalregistrationpage/`;
 export const finalRegistrationRoute = `${host}/academic-procedures/api/stu/final_registration/`;
+export const studentListRoute = `${host}/academic-procedures/api/acad/student_list/`;
+export const courseListRoute = `${host}/academic-procedures/api/acad/course_list/`;
+export const verifyRegistrationRoute = `${host}/academic-procedures/api/acad/verify_registration/`;
+export const batchesRoute = `${host}/programme_curriculum/api/admin_batches/`;
